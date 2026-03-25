3a87cccad5f4a6e4690e25f233c5a47d26607fad
gamesalone85 <257934905+gamesalone85@users.noreply.github.com>
2026-03-24 19:53:27 -0600

    Update layout.js

--- layout.js
+++ layout.js
@@ -1,9 +1,9 @@
 function loadComponent(id, file) {
-  fetch(file)
-    .then(res => res.text())
-    .then(data => {
-      document.getElementById(id).innerHTML = data;
-    });
+    fetch(file)
+        .then(res => res.text())
+        .then(data => {
+            document.getElementById(id).innerHTML = data;
+        });
 }
 
 loadComponent("header", "components/header.html");
