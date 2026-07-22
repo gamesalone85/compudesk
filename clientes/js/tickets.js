069c70400bb47e0b56a7bee331da758649780e98
gamesalone85 <257934905+gamesalone85@users.noreply.github.com>
2026-07-21 18:56:17 -0600

    Add tickets.js for ticket management module

---
+++ clientes/js/tickets.js
@@ -0,0 +1,40 @@
+/*
+========================================
+COMPU DESK
+Mis Tickets JS
+Versión 1.0
+========================================
+*/
+
+
+document.addEventListener(
+"DOMContentLoaded",
+function(){
+
+
+console.log(
+"Modulo Mis Tickets cargado"
+);
+
+
+
+const lista =
+document.getElementById(
+"listaTickets"
+);
+
+
+
+if(lista){
+
+
+console.log(
+"Lista preparada para Firebase"
+);
+
+
+}
+
+
+
+});
